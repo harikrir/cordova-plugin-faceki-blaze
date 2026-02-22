8c9ba32165084230e888dc1592b098606afa3387
harikrir <hkrishnan254@gmail.com>
2026-02-22 22:38:06 +0530

    Update FacekiBlaze.swift

--- src/ios/FacekiBlaze.swift
+++ src/ios/FacekiBlaze.swift
@@ -1,18 +1,22 @@
 import Foundation
+#if canImport(Cordova)
+import Cordova
+#endif
 import FACEKI_BLAZE_IOS
 
 @objc(FacekiBlaze) class FacekiBlaze : CDVPlugin {
     @objc(startVerification:)
     func startVerification(command: CDVInvokedUrlCommand) {
         let url = command.arguments[0] as? String ?? ""
-        let workflowId = command.arguments[1] as? String ?? "" // Get workflowId
-        
+        let workflowId = command.arguments[1] as? String ?? ""
+
         DispatchQueue.main.async {
             let sdkVC = Logger.initiateSMSDK(
                 verificationLink: url,
-                workflowId: workflowId, // Now using the passed workflowId
+                workflowId: workflowId,
                 setOnComplete: { data in
-                    let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: data as? [String: Any])
+                    let result = CDVPluginResult(status: CDVCommandStatus_OK,
+                                                 messageAs: data as? [String: Any])
                     self.commandDelegate!.send(result, callbackId: command.callbackId)
                 },
                 onRedirectBack: {
