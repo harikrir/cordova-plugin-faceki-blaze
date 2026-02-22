481fcad9ed8303fc5a1fac16f99f63782ba4b13a
harikrir <hkrishnan254@gmail.com>
2026-02-22 20:41:29 +0530

    Refactor startFaceki method for KYC verification

--- src/android/FacekiBlaze.java
+++ src/android/FacekiBlaze.java
@@ -2,11 +2,9 @@ package com.faceki.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import com.faceki.blaze_android_sdk.FaceKi;
-import com.faceki.blaze_android_sdk.KycResponseHandler;
 import com.faceki.blaze_android_sdk.VerificationResult;
 
 public class FacekiBlaze extends CordovaPlugin {
@@ -14,31 +12,22 @@ public class FacekiBlaze extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("startVerification")) {
             String url = args.getString(0);
-            this.startFaceki(url, callbackContext);
-            return true;
-        }
-        return false;
-    }
-
-    private void startFaceki(String url, final CallbackContext callbackContext) {
-        cordova.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
+            cordova.getActivity().runOnUiThread(() -> {
                 FaceKi.INSTANCE.startKycVerification(
                     cordova.getActivity(),
                     url,
-                    "outsystems_record",
-                    new KycResponseHandler() {
-                        @Override
-                        public void handleKycResponse(String json, VerificationResult result) {
-                            if (result instanceof VerificationResult.ResultOk) {
-                                callbackContext.success(json);
-                            } else {
-                                callbackContext.error("Verification Canceled or Failed");
-                            }
+                    "os_request",
+                    (json, result) -> {
+                        if (result instanceof VerificationResult.ResultOk) {
+                            callbackContext.success(json);
+                        } else {
+                            callbackContext.error("Verification failed");
                         }
                     }
                 );
-            }
-        });
+            });
+            return true;
+        }
+        return false;
     }
 }
