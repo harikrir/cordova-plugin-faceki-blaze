6f6ff33bd0ffcd131d460e57ef697faae8f08e81
harikrir <hkrishnan254@gmail.com>
2026-02-22 22:09:34 +0530

    Update faceki-blaze.js

--- www/faceki-blaze.js
+++ www/faceki-blaze.js
@@ -1,13 +1,9 @@
 var exec = require('cordova/exec');
 
-/**
- * FacekiBlaze JavaScript Interface
- */
 var FacekiBlaze = {
-    startVerification: function (verificationUrl, success, error) {
-        // The third parameter 'FacekiBlaze' must match the <feature name="..."> in plugin.xml
-        // The fourth parameter 'startVerification' must match the action string in your Native code
-        exec(success, error, 'FacekiBlaze', 'startVerification', [verificationUrl]);
+    // Now accepting both URL and WorkflowID
+    startVerification: function (verificationUrl, workflowId, success, error) {
+        exec(success, error, 'FacekiBlaze', 'startVerification', [verificationUrl, workflowId]);
     }
 };
 
