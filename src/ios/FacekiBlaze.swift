34c4c0151b55b4d9ff0318ca766f69df974bd670
harikrir <hkrishnan254@gmail.com>
2026-02-22 22:10:47 +0530

    Use passed workflowId in startVerification method

--- src/ios/FacekiBlaze.swift
+++ src/ios/FacekiBlaze.swift
@@ -5,11 +5,12 @@ import FACEKI_BLAZE_IOS
     @objc(startVerification:)
     func startVerification(command: CDVInvokedUrlCommand) {
         let url = command.arguments[0] as? String ?? ""
+        let workflowId = command.arguments[1] as? String ?? "" // Get workflowId
         
         DispatchQueue.main.async {
             let sdkVC = Logger.initiateSMSDK(
                 verificationLink: url,
-                workflowId: "",
+                workflowId: workflowId, // Now using the passed workflowId
                 setOnComplete: { data in
                     let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: data as? [String: Any])
                     self.commandDelegate!.send(result, callbackId: command.callbackId)
