442e9ec2b8b43c449df40aceeaed182a2faf3902
harikrir <hkrishnan254@gmail.com>
2026-02-22 22:10:18 +0530

    Pass workflowId to startKycVerification method

--- src/android/FacekiBlaze.java
+++ src/android/FacekiBlaze.java
@@ -12,16 +12,18 @@ public class FacekiBlaze extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("startVerification")) {
             String url = args.getString(0);
+            String workflowId = args.optString(1, ""); // Get workflowId (arg index 1)
+            
             cordova.getActivity().runOnUiThread(() -> {
                 FaceKi.INSTANCE.startKycVerification(
                     cordova.getActivity(),
                     url,
-                    "os_request",
+                    workflowId, // Passing the workflowId here
                     (json, result) -> {
                         if (result instanceof VerificationResult.ResultOk) {
                             callbackContext.success(json);
                         } else {
-                            callbackContext.error("Verification failed");
+                            callbackContext.error("Verification failed or cancelled");
                         }
                     }
                 );
