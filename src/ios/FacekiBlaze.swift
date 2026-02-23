9587d8326d93321a05feb0ef8fb44c1ac5759789
harikrir <hkrishnan254@gmail.com>
2026-02-23 13:15:42 +0300

    Update FacekiBlaze.swift

--- src/ios/FacekiBlaze.swift
+++ src/ios/FacekiBlaze.swift
@@ -7,19 +7,19 @@ import FACEKI_BLAZE_IOS
 @objc(FacekiBlaze) class FacekiBlaze : CDVPlugin {
     @objc(startVerification:)
     func startVerification(command: CDVInvokedUrlCommand) {
-        let url = command.arguments[0] as? String ?? ""
+        let linkId = command.arguments[0] as? String ?? ""
         let workflowId = command.arguments[1] as? String ?? ""
 
         DispatchQueue.main.async {
             let sdkVC = Logger.initiateSMSDK(
-                verificationLink: url,
+                verificationLink: linkId,
                 workflowId: workflowId,
                 setOnComplete: { data in
                     let result = CDVPluginResult(status: CDVCommandStatus_OK,
                                                  messageAs: data as? [String: Any])
                     self.commandDelegate!.send(result, callbackId: command.callbackId)
                 },
-                onRedirectBack: {
+                redirectBack: {     // <-- FACEKI docs use `redirectBack`
                     self.viewController.dismiss(animated: true, completion: nil)
                 }
             )
