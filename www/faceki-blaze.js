3017c9a80355e644ea29f738b8ce808f19cb8537
harikrir <hkrishnan254@gmail.com>
2026-05-17 17:00:27 +0300

    Update faceki-blaze.js

--- www/faceki-blaze.js
+++ www/faceki-blaze.js
@@ -1,11 +1,6 @@
 var exec = require('cordova/exec');
 
 var FacekiBlaze = {
-  /**
-   * @param {string} verificationLink - UUID from FACEKI "Generate KYC Link" API (response.data)
-   * @param {string} recordIdentifier  - Optional identifier you track on Android (pass "" on iOS if not used)
-   * @returns {Promise<string>} JSON string payload from FACEKI
-   */
   startVerification: function (verificationLink, recordIdentifier) {
     return new Promise(function (resolve, reject) {
       exec(resolve, reject, 'FacekiBlaze', 'startVerification', [verificationLink, recordIdentifier]);
